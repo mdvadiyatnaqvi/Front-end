105b46c0d3a0ca014a515401bc6db346649ac7bb
mdvadiyatnaqvi <mdvadiyatnaqvi@gmail.com>
2023-12-05 14:38:36 +0530

    ui updated

--- React/todo-app/src/components/TodoItem.jsx
+++ React/todo-app/src/components/TodoItem.jsx
@@ -5,7 +5,7 @@ const TodoItem = ({ todoName, todoDate }) => {
                 <div className="col-4">{todoName}</div>
                 <div className="col-4">{todoDate}</div>
                 <div className="col-4">
-                    <button type="button" className="btn btn-danger">Delete</button>
+                    <button type="button" className="btn btn-danger spec">Delete</button>
                 </div>
             </div>
         </div>
