b12cc731cd76530ac4871121d220d64104711b81
Md Vadiyat Naqvi <mdvadiyatnaqvi@gmail.com>
2025-02-17 13:26:47 +0530

    code removed

--- React/bharat-clock/src/components/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Heading = () => {
-    return (
-        <center className='clock-heading'>Bharat Clock</center>
-    )
-}
-
-export default Heading
--- React/concepts/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>React Concepts</div>
-  )
-}
-
-export default App
